23b6d3720b2e76ead72e89e899cfd5a77679bb59
Zac Miller <zmiller1@gmail.com>
2016-10-29 17:05:36 -0400

    Update initial_data_loader to load remaining models
    Updated the initial_data_loader to load data for Technicians, FeedbackLevels and Feedback.  The loader is ran by the initial data migration so no changes are needed to get the data into the db.
    
    Updated all models to have a __str__ method.

--- clean/fixtures/loaders/initial_data_loader.py
+++ clean/fixtures/loaders/initial_data_loader.py
@@ -2,6 +2,7 @@ import os
 import django
 from django.conf import settings
 django.setup()
+from django.contrib.auth.models import User
 from collections import namedtuple
 from datetime import datetime
 from clean import models
@@ -12,7 +13,8 @@ data_path = os.path.join(settings.BASE_DIR,
 Row = namedtuple('Row', 'service_ceo_id feedback_comment tech_1 tech_2 tech_3 tech_4 job_id contact_id quality_inspector feedback_score quality_check_score job_scheduled quality_feedback_comment')
 
 f = open(data_path, 'r')
-data = [Row(*row.strip().split(',')) for row in f.readlines() if len(row.strip().split(',')) == 13]
+data = [Row(*row.strip().split(',')) for row in f.readlines()
+        if len(row.strip().split(',')) == 13]
 f.close()
 
 jobs = [(int(row.job_id), datetime.strptime(row.job_scheduled, '%m/%d/%Y'))
@@ -22,3 +24,29 @@ for job in jobs:
     new_job = models.Job(job_id=job[0], scheduled=job[1], completed=job[1])
     new_job.save()
 
+techs =list(set([tech for row in data[1:]
+                 for tech in [row.tech_1, row.tech_2, row.tech_3, row.tech_4]
+                 if tech != '']))
+
+counter = 0
+for tech in techs:
+    user = User.objects.create_user(tech, 'tech{}@tech.com'.format(counter))
+    user.save()
+    new_tech = models.Technician(name=tech, user=user).save()
+    counter += 1
+
+levels = list(set([row.feedback_score for row in data[1:]]))
+
+for level in levels:
+    new_feedback_score = models.FeedbackLevel(value=int(level), title=level).save()
+
+for row in data[1:]:
+    if row.job_id.isnumeric():
+        feedback_score = models.FeedbackLevel.objects.filter(title=row.feedback_score).first()
+        job = models.Job.objects.filter(job_id=int(row.job_id)).first()
+        for tech in [row.tech_1, row.tech_2, row.tech_3, row.tech_4]:
+            if tech != '':
+                tech = models.Technician.objects.filter(name=tech).first()
+                new_feedback = models.Feedback(job=job,
+                                               tech=tech,
+                                               level=feedback_score).save()
--- clean/models.py
+++ clean/models.py
@@ -7,10 +7,16 @@ class Job(models.Model):
     completed = models.DateTimeField()
     job_id = models.IntegerField()
 
+    def __str__(self):
+        return "Job {} - job_id: {} Scheduled: {}  Completed: {}".format(self.pk, self.job_id, self.scheduled, self.completed)
+
 
 class Technician(models.Model):
     name = models.CharField(max_length=60)
     user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "Tech: {} email: {}".format(self.name, self.user.email)
 
 
 class Contact(models.Model):
@@ -20,13 +26,22 @@ class Contact(models.Model):
     address = models.TextField(max_length=240)
     email = models.EmailField()
 
+    def __str__(self):
+        return "Contact: {} {} - email: {}".format(self.name_first, self.name_last, self.user.email)
+
 
 class FeedbackLevel(models.Model):
     title = models.CharField(max_length=140)
     value = models.IntegerField()
+
+    def __str__(self):
+        return "Level: {} - Value {}".format(self.title, self.value)
 
 
 class Feedback(models.Model):
     job = models.ForeignKey(Job)
     tech = models.ForeignKey(Technician)
     level = models.ForeignKey(FeedbackLevel)
+
+    def __str__(self):
+        return "Feedback - Job {} - {} - {}".format(self.job.job_id, self.tech.name, self.level.title)

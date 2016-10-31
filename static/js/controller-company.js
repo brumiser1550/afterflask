0322fbcaf2eb3e8434061ac7504bb0b5b179dfcd
Brandon Rumiser <brumiser1550@gmail.com>
2016-10-30 21:49:36 -0700

    Added angular code to display feedback list on home page

--- static/js/controller-company.js
+++ static/js/controller-company.js
@@ -2,21 +2,19 @@
  * Created by Brandon on 10/3/2016.
  */
 
-cleanApp.controller('companyController', function ($scope) {
-    $scope.companies = {
-        serviceceoID: 391,
-        feedbackComment: "Great job....thank you!",
-        tech1: "0",
-        qualityInspector: "",
-        contactId: 4608,
-        serviceceoJobID: 56690,
-        tech4: 0,
-        tech3: 0,
-        tech2: 0,
-        feedbackScore: 4,
-        qualityCheckScore: 0,
-        cleaningAppointmentDate: "4/14/2016",
-        qualityFeedbakComment: 0
-    };
-    console.log($scope);
+cleanApp.controller('companyController', function ($scope, $http) {
+    $scope.feedback = get_feedback();
+
+    function get_feedback() {
+        $http({
+            method: 'GET',
+            url: '/api/v1/feedback/'
+        }).then(function (response) {
+            $scope.feedback = response.data;
+            console.log($scope.feedback);
+        }, function (response, error_code) {
+            console.log(response);
+        });
+    }
+
 });

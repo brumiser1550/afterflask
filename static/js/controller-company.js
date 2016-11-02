05ececeecaee7bb7a0afa755c873121fd2b5a2fc
Brandon Rumiser <brumiser1550@gmail.com>
2016-11-01 19:00:15 -0700

    Updated display to pull from jobs API

--- static/js/controller-company.js
+++ static/js/controller-company.js
@@ -18,16 +18,16 @@ cleanApp.controller('companyController', function ($scope, $http) {
                 value.count = 0;
                 $scope.levels[value.value] = value;
             });
-            get_feedback();
+            get_overview();
         }, function (response, error_code) {
             console.log(response);
         });
     }
 
-    function get_feedback() {
+    function get_overview() {
         $http({
             method: 'GET',
-            url: '/api/v1/feedback/'
+            url: '/api/v1/jobs/'
         }).then(function (response) {
             $scope.feedback = response.data;
             get_totals();

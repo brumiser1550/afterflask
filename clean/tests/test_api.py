eb036708fa59bb4a0a602e21ef9e02c8feacf000
Zac Miller <zmiller1@gmail.com>
2016-11-21 16:31:15 -0500

    Update tests for existing work
    All pages (including api endpoints) should:
    1. Resolve to the proper View
    2. Return a 200 (login_required urls return 302 if not logged in)
    
    In addition to 1 and 2 the non-api views/urls should:
    3. Use the appropriate template
    4. Contain the proper context data (if any special context data is passed)

---
+++ clean/tests/test_api.py
@@ -0,0 +1,84 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve, reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory, force_authenticate
+from ..api import views
+from ..api import serializers
+from .. import models
+
+
+class JsonViewTests(TestCase):
+    """ All pages should:
+
+        1. Resolve to the proper View
+        2. Return a 200 (login_required urls return 302 if not logged in)
+
+    """
+    fixtures = ['data/tests_auth_users.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super(JsonViewTests, cls).setUpClass()
+        cls.factory = APIRequestFactory()
+        cls.test_user = User.objects.filter(pk=1)
+
+    def get_request(self, method='GET', authed=True):
+        request_method = getattr(self.factory, method.lower())
+        request = request_method("")
+        if authed:
+            force_authenticate(request, self.test_user.first())
+        return request
+
+    def test_jobs_collection_resolves(self):
+        url = reverse('api_clean:job_collection',
+                      args=[])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.JobCollection.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_feedback_collection_resolves(self):
+        url = reverse('api_clean:feedback_collection',
+                      args=[])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.FeedbackCollection.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_feedback_detail_resolves(self):
+        url = reverse('api_clean:feedback_detail',
+                      args=[1])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.FeedbackDetail.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_feedback_level_collection_resolves(self):
+        url = reverse('api_clean:feedback_level_collection',
+                               args=[])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.FeedbackLevelCollection.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_technician_collection_resolves(self):
+        url = reverse('api_clean:technician_collection',
+                      args=[])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.TechnicianCollection.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_technician_detail_resolves(self):
+        url = reverse('api_clean:technician_detail',
+                      args=[1])
+        view = resolve(url)
+        self.assertEqual(view.func.__name__,
+                         views.TechnicianDetail.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
--- clean/tests/test_unit.py
+++ clean/tests/test_unit.py
@@ -12,15 +12,110 @@ class PageTest(TestCase):
         3. Use the appropriate template
         4. Contain the proper context data
     """
-    fixtures = []
+    fixtures = ['data/tests_auth_users.json']
 
-    def test_mainpage(self):
+    def test_company_page(self):
         """
-        Test the mainpage url and view
+        Test the company url and view
         """
-        url = reverse('clean:mainpage')
+        url = reverse('clean:company')
         v = resolve(url)
-        self.assertEqual(v.func.__name__, views.MainView.__name__)
+        self.assertEqual(v.func.__name__, views.CompanyView.__name__)
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'clean/home.html')
+        self.assertTemplateUsed(response, 'clean/company.html')
+
+    def test_leave_feedback_page(self):
+        """
+        Test the leave-feedback url and view
+        """
+        url = reverse('clean:leave-feedback')
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.LeaveFeedbackView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/feedback.html')
+
+    def test_technician_list_page(self):
+        """
+        Test the technicians url and view
+        """
+        url = reverse('clean:technicians')
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.TechnicianListView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/technicians_list.html')
+
+    def test_technician_detail_page(self):
+        """
+        Test the technicians detail url and view
+        """
+        url = reverse('clean:technician', args=[1])
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.TechnicianDetailView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/technicians_detail.html')
+
+    def test_job_list_page(self):
+        """
+        Test the job list url and view
+        """
+        url = reverse('clean:jobs')
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.JobListView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/jobs_list.html')
+
+    def test_job_detail_page(self):
+        """
+        Test the job detail url and view
+        """
+        url = reverse('clean:job', args=[1])
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.JobDetailView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/jobs_detail.html')
+
+    def test_feedback_list_page(self):
+        """
+        Test the feedback list url and view
+        """
+        url = reverse('clean:feedbacks')
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.FeedbackListView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/feedback_list.html')
+
+    def test_feedback_detail_page(self):
+        """
+        Test the feedback detail url and view
+        """
+        url = reverse('clean:feedback', args=[1])
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.FeedbackDetailView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='test_admin_user', password='razzle01')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'clean/feedback_detail.html')
